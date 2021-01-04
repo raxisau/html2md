5ad07583dca5356e5bf93d64d8f4289575bb8cdb
Brett Dutton <brett@b2bconsultancy.asia>
2021-01-04 09:05:18 +0700

    Updated

--- cmd/html2md/html2md.go
+++ cmd/html2md/html2md.go
@@ -1,40 +1,14 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
-	"os/exec"
-	"runtime"
-	"strings"
 
 	"bitbucket.org/jackbooted/html2markdown/internal/pkg/html2md"
 )
 
-var (
-	guesslang = flag.String("g", "", "guesslang")
-	option    *html2md.Option
-)
-
-func guesslanger(code string) (string, error) {
-	var cmd *exec.Cmd
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("cmd", "/c", *guesslang)
-	} else {
-		cmd = exec.Command("sh", "-c", *guesslang)
-	}
-	cmd.Stdin = strings.NewReader(code)
-	b, err := cmd.CombinedOutput()
-	return strings.ToLower(strings.TrimSpace(string(b))), err
-}
-
 func main() {
-	flag.Parse()
-	if *guesslang != "" {
-		option = &html2md.Option{GuessLang: guesslanger}
-	}
-	option := &html2md.Option{GuessLang: guesslanger}
-	if err := html2md.Convert(os.Stdout, os.Stdin, option); err != nil {
+	if err := html2md.Convert(os.Stdout, os.Stdin); err != nil {
 		log.Fatal(err)
 	}
 }
--- internal/pkg/html2md/convert.go
+++ internal/pkg/html2md/convert.go
@@ -12,6 +12,12 @@ import (
 	"golang.org/x/net/html"
 )
 
+const (
+	displayStyle    = false
+	displayScript   = false
+	displayComments = false
+)
+
 func isChildOf(node *html.Node, name string) bool {
 	node = node.Parent
 	return node != nil && node.Type == html.ElementNode && strings.ToLower(node.Data) == name
@@ -39,7 +45,7 @@ func attr(node *html.Node, key string) string {
 	return ""
 }
 
-func br(node *html.Node, w io.Writer, option *Option) {
+func br(node *html.Node, w io.Writer) {
 	node = node.PrevSibling
 	if node == nil {
 		return
@@ -58,7 +64,7 @@ func br(node *html.Node, w io.Writer, option *Option) {
 	}
 }
 
-func table(node *html.Node, w io.Writer, option *Option) {
+func table(node *html.Node, w io.Writer) {
 	for tr := node.FirstChild; tr != nil; tr = tr.NextSibling {
 		if tr.Type == html.ElementNode && strings.ToLower(tr.Data) == "tbody" {
 			node = tr
@@ -78,7 +84,7 @@ func table(node *html.Node, w io.Writer, option *Option) {
 					continue
 				}
 				var buf bytes.Buffer
-				walk(th, &buf, 0, option)
+				walk(th, &buf, 0)
 				cols = append(cols, buf.String())
 			}
 			if len(cols) > 0 {
@@ -92,7 +98,7 @@ func table(node *html.Node, w io.Writer, option *Option) {
 				continue
 			}
 			var buf bytes.Buffer
-			walk(td, &buf, 0, option)
+			walk(td, &buf, 0)
 			cols = append(cols, buf.String())
 		}
 		rows = append(rows, cols)
@@ -155,7 +161,7 @@ var emptyElements = []string{
 	"wbr",
 }
 
-func raw(node *html.Node, w io.Writer, option *Option) {
+func raw(node *html.Node, w io.Writer) {
 	switch node.Type {
 	case html.ElementNode:
 		fmt.Fprintf(w, "<%s", node.Data)
@@ -175,7 +181,7 @@ func raw(node *html.Node, w io.Writer, option *Option) {
 		} else {
 			fmt.Fprint(w, ">")
 			for c := node.FirstChild; c != nil; c = c.NextSibling {
-				raw(c, w, option)
+				raw(c, w)
 			}
 			fmt.Fprintf(w, "</%s>", node.Data)
 		}
@@ -184,27 +190,27 @@ func raw(node *html.Node, w io.Writer, option *Option) {
 	}
 }
 
-func bq(node *html.Node, w io.Writer, option *Option) {
+func bq(node *html.Node, w io.Writer) {
 	if node.Type == html.TextNode {
 		fmt.Fprint(w, strings.Replace(node.Data, "\u00a0", " ", -1))
 	} else {
 		for c := node.FirstChild; c != nil; c = c.NextSibling {
-			bq(c, w, option)
+			bq(c, w)
 		}
 	}
 }
 
-func pre(node *html.Node, w io.Writer, option *Option) {
+func pre(node *html.Node, w io.Writer) {
 	if node.Type == html.TextNode {
 		fmt.Fprint(w, node.Data)
 	} else {
 		for c := node.FirstChild; c != nil; c = c.NextSibling {
-			pre(c, w, option)
+			pre(c, w)
 		}
 	}
 }
 
-func walk(node *html.Node, w io.Writer, nest int, option *Option) {
+func walk(node *html.Node, w io.Writer, nest int) {
 	if node.Type == html.TextNode {
 		if strings.TrimSpace(node.Data) != "" {
 			text := regexp.MustCompile(`[[:space:]][[:space:]]*`).ReplaceAllString(strings.Trim(node.Data, "\t\r\n"), " ")
@@ -215,80 +221,70 @@ func walk(node *html.Node, w io.Writer, nest int, option *Option) {
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
 		switch c.Type {
 		case html.CommentNode:
-			fmt.Fprint(w, "<!--")
-			fmt.Fprint(w, c.Data)
-			fmt.Fprint(w, "-->\n")
+			if displayComments {
+				fmt.Fprint(w, "<!--")
+				fmt.Fprint(w, c.Data)
+				fmt.Fprint(w, "-->\n")
+			}
 		case html.ElementNode:
 			switch strings.ToLower(c.Data) {
 			case "a":
 				fmt.Fprint(w, "[")
-				walk(c, w, nest, option)
+				walk(c, w, nest)
 				fmt.Fprint(w, "]("+attr(c, "href")+")")
 			case "b", "strong":
 				fmt.Fprint(w, "**")
-				walk(c, w, nest, option)
+				walk(c, w, nest)
 				fmt.Fprint(w, "**")
 			case "i", "em":
 				fmt.Fprint(w, "_")
-				walk(c, w, nest, option)
+				walk(c, w, nest)
 				fmt.Fprint(w, "_")
 			case "del":
 				fmt.Fprint(w, "~~")
-				walk(c, w, nest, option)
+				walk(c, w, nest)
 				fmt.Fprint(w, "~~")
 			case "br":
-				br(c, w, option)
+				br(c, w)
 				fmt.Fprint(w, "\n\n")
 			case "p":
-				br(c, w, option)
-				walk(c, w, nest, option)
-				br(c, w, option)
+				br(c, w)
+				walk(c, w, nest)
+				br(c, w)
 				fmt.Fprint(w, "\n\n")
 			case "code":
 				if !isChildOf(c, "pre") {
 					fmt.Fprint(w, "`")
-					pre(c, w, option)
+					pre(c, w)
 					fmt.Fprint(w, "`")
 				}
 			case "pre":
-				br(c, w, option)
+				br(c, w)
 				var buf bytes.Buffer
-				pre(c, &buf, option)
-				var lang string
-				if option != nil && option.GuessLang != nil {
-					if guess, err := option.GuessLang(buf.String()); err == nil {
-						lang = guess
-					}
-				}
-				fmt.Fprint(w, "```"+lang+"\n")
+				pre(c, &buf)
+				fmt.Fprint(w, "```\n")
 				fmt.Fprint(w, buf.String())
 				if !strings.HasSuffix(buf.String(), "\n") {
 					fmt.Fprint(w, "\n")
 				}
 				fmt.Fprint(w, "```\n\n")
 			case "div":
-				br(c, w, option)
-				walk(c, w, nest, option)
+				br(c, w)
+				walk(c, w, nest)
 				fmt.Fprint(w, "\n")
 			case "blockquote":
-				br(c, w, option)
+				br(c, w)
 				var buf bytes.Buffer
 				if hasClass(c, "code") {
-					bq(c, &buf, option)
-					var lang string
-					if option != nil && option.GuessLang != nil {
-						if guess, err := option.GuessLang(buf.String()); err == nil {
-							lang = guess
-						}
-					}
-					fmt.Fprint(w, "```"+lang+"\n")
+					bq(c, &buf)
+					fmt.Fprint(w, "```\n")
 					fmt.Fprint(w, strings.TrimLeft(buf.String(), "\n"))
 					if !strings.HasSuffix(buf.String(), "\n") {
 						fmt.Fprint(w, "\n")
 					}
 					fmt.Fprint(w, "```\n\n")
 				} else {
-					walk(c, &buf, nest+1, option)
+					walk(c, &buf, nest+1)
 
 					if lines := strings.Split(strings.TrimSpace(buf.String()), "\n"); len(lines) > 0 {
 						for _, l := range lines {
@@ -298,9 +294,9 @@ func walk(node *html.Node, w io.Writer, nest int, option *Option) {
 					}
 				}
 			case "ul", "ol":
-				br(c, w, option)
+				br(c, w)
 				var buf bytes.Buffer
-				walk(c, &buf, 1, option)
+				walk(c, &buf, 1)
 				if lines := strings.Split(strings.TrimSpace(buf.String()), "\n"); len(lines) > 0 {
 					for i, l := range lines {
 						if i > 0 {
@@ -311,95 +307,56 @@ func walk(node *html.Node, w io.Writer, nest int, option *Option) {
 					fmt.Fprint(w, "\n")
 				}
 			case "li":
-				br(c, w, option)
+				br(c, w)
 				if isChildOf(c, "ul") {
 					fmt.Fprint(w, "* ")
 				} else if isChildOf(c, "ol") {
 					n++
 					fmt.Fprint(w, fmt.Sprintf("%d. ", n))
 				}
-				walk(c, w, nest, option)
+				walk(c, w, nest)
 				fmt.Fprint(w, "\n")
 			case "h1", "h2", "h3", "h4", "h5", "h6":
-				br(c, w, option)
+				br(c, w)
 				fmt.Fprint(w, strings.Repeat("#", int(rune(c.Data[1])-rune('0')))+" ")
-				walk(c, w, nest, option)
+				walk(c, w, nest)
 				fmt.Fprint(w, "\n\n")
 			case "img":
 				fmt.Fprint(w, "!["+attr(c, "alt")+"]("+attr(c, "src")+")")
 			case "hr":
-				br(c, w, option)
+				br(c, w)
 				fmt.Fprint(w, "\n---\n\n")
 			case "table":
-				br(c, w, option)
-				table(c, w, option)
+				br(c, w)
+				table(c, w)
 			case "style":
-				if option != nil && option.Style {
-					br(c, w, option)
-					raw(c, w, option)
+				if displayStyle {
+					br(c, w)
+					raw(c, w)
 					fmt.Fprint(w, "\n\n")
 				}
 			case "script":
-				if option != nil && option.Script {
-					br(c, w, option)
-					raw(c, w, option)
+				if displayScript {
+					br(c, w)
+					raw(c, w)
 					fmt.Fprint(w, "\n\n")
 				}
 			default:
-				if option == nil || option.CustomRules == nil {
-					walk(c, w, nest, option)
-					break
-				}
-
-				foundCustom := false
-				for _, cr := range option.CustomRules {
-					if tag, customWalk := cr.Rule(walk); strings.ToLower(c.Data) == tag {
-						customWalk(c, w, nest, option)
-						foundCustom = true
-					}
-				}
-
-				if foundCustom {
-					break
-				}
-				walk(c, w, nest, option)
+				walk(c, w, nest)
 			}
 		default:
-			walk(c, w, nest, option)
+			walk(c, w, nest)
 		}
 	}
 }
 
-// WalkFunc type is an signature for functions traversing HTML nodes
-type WalkFunc func(node *html.Node, w io.Writer, nest int, option *Option)
-
-// CustomRule is an interface to define custom conversion rules
-//
-// Rule method accepts `next WalkFunc` as an argument, which `customRule` should call
-// to let walk function continue parsing the content inside the HTML tag.
-// It returns a tagName to indicate what HTML element this `customRule` handles and the `customRule`
-// function itself, where conversion logic should reside.
-//
-// See example TestRule implementation in godown_test.go
-type CustomRule interface {
-	Rule(next WalkFunc) (tagName string, customRule WalkFunc)
-}
-
-// Option is optional information for Convert.
-type Option struct {
-	GuessLang   func(string) (string, error)
-	Script      bool
-	Style       bool
-	CustomRules []CustomRule
-}
-
 // Convert convert HTML to Markdown. Read HTML from r and write to w.
-func Convert(w io.Writer, r io.Reader, option *Option) error {
+func Convert(w io.Writer, r io.Reader) error {
 	doc, err := html.Parse(r)
 	if err != nil {
 		return err
 	}
-	walk(doc, w, 0, option)
+	walk(doc, w, 0)
 	fmt.Fprint(w, "\n")
 	return nil
 }
